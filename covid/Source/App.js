fc41004316715dec2165663e518e66610ae69ff3
bayashi <naoya_nstyle@hotmail.co.jp>
2020-03-21 03:06:28 +0900

    add

--- covid/Source/App.js
+++ covid/Source/App.js
@@ -1,1 +1,7 @@
-var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
+viewer.dataSources.add(Cesium.GeoJsonDataSource.load('pref_covid.geojson', {
+  stroke: Cesium.Color.HOTPINK,
+  fill: Cesium.Color.PINK,
+  strokeWidth: 3,
+  markerSymbol: '?'
+}));
