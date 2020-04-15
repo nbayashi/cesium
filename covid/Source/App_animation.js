b724e6b5ee72fad5817e74b841bbe0681389b499
bayashi <naoya_nstyle@hotmail.co.jp>
2020-04-16 01:08:14 +0900

    Update App_animation.js

--- covid/Source/App_animation.js
+++ covid/Source/App_animation.js
@@ -1,7 +1,39 @@
 var viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate : true
   });
-var czml =Cesium.CzmlDataSource.load('https://nbayashi.github.io/cesium/covid/Source/total.czml')
-viewer.dataSources.add(czml);
 
-viewer.zoomTo(czml);
+var czml = [{
+   "id" : "document",
+   "name" : "CZML",
+   "version" : "1.0"
+ },
+ {
+   "id" : "shape1",
+   "name" : "TOKYO",
+   "position" : {
+     "cartographicDegrees" : [139.77, 35.68, 20000.0]
+   },
+   "ellipse" : {
+     "semiMinorAxis" : 50000.0,
+     "semiMajorAxis" : 50000.0,
+     "extrudedHeight" : {
+       "number" : 20000.0
+     },
+     "rotation" : {
+       "number" : 0.8
+     },
+     "material" : {
+       "solidColor" : {
+         "color" : {
+           "rgba" : [0, 255, 0, 100]
+         }
+       }
+     },
+     "outline" : true,
+     "outlineColor" : {
+       "rgba" : [255, 0, 0, 0]
+     }
+   }
+ }];
+ 
+viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
