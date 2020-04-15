bc1811671aa3157cfaf359d288a44bc2e0686833
bayashi <naoya_nstyle@hotmail.co.jp>
2020-04-16 01:16:04 +0900

    Update App_animation.js

--- covid/Source/App_animation.js
+++ covid/Source/App_animation.js
@@ -2,6 +2,6 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate : true
   });
 
-var czml = Cesium.CzmlDataSource.load(czml);
+var czml = Cesium.CzmlDataSource.load('Source/total.czml');
  
 viewer.dataSources.add(czml);
