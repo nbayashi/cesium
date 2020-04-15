325d9a4fc184de08cc8f77ad843adf011329c254
bayashi <naoya_nstyle@hotmail.co.jp>
2020-04-16 01:47:29 +0900

    Update App_toolbar.js

--- covid/Source/App_toolbar.js
+++ covid/Source/App_toolbar.js
@@ -7,14 +7,43 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 
   document.getElementById("累積感染者数").addEventListener("click",function () {
     viewer.dataSources.removeAll();
-    var promise =Cesium.CzmlDataSource.load('https://nbayashi.github.io/cesium/covid/Source/total.czml');
-    promise.then(function(dataSource) {
-      viewer.dataSources.add(dataSource);
-
-
-    });
-    //   viewer.dataSources.add(Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10.geojson'));
     
+    var czml = [{
+      "id" : "document",
+      "name" : "CZML",
+      "version" : "1.0"
+    },
+    {
+      "id" : "shape1",
+      "name" : "TOKYO",
+      "position" : {
+        "cartographicDegrees" : [139.77, 35.68, 20000.0]
+      },
+      "ellipse" : {
+        "semiMinorAxis" : 50000.0,
+        "semiMajorAxis" : 50000.0,
+        "extrudedHeight" : {
+          "number" : 20000.0
+        },
+        "rotation" : {
+          "number" : 0.8
+        },
+        "material" : {
+          "solidColor" : {
+            "color" : {
+              "rgba" : [0, 255, 0, 100]
+            }
+          }
+        },
+        "outline" : true,
+        "outlineColor" : {
+          "rgba" : [255, 0, 0, 0]
+        }
+      }
+    }];
+   viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+
+
     });
 
 
