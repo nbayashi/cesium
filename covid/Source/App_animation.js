750dc0f1cdd810c907a2d9fe8fc0924034ed68f1
bayashi <naoya_nstyle@hotmail.co.jp>
2020-04-15 23:47:26 +0900

    czml

--- covid/Source/App_animation.js
+++ covid/Source/App_animation.js
@@ -3,6 +3,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate : true
   });
-  var dataSourcePromise = Cesium.CzmlDataSource.load("https://nbayashi.github.io/cesium/covid/Source/total.czml");
-  viewer.dataSources.add(dataSourcePromise);
-  viewer.zoomTo(dataSourcePromise);
+  var czml = Cesium.CzmlDataSource.load("total.czml")
+  var dataSourcePromise = viewer.dataSources.add(czml);
+
+  viewer.zoomTo(czml);
