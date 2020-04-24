945baf4cfa8bdd3c776d204f1f90053e72b65870
bayashi <naoya_nstyle@hotmail.co.jp>
2020-04-25 02:48:18 +0900

    add0424

--- covid/Source/App_toolbar.js
+++ covid/Source/App_toolbar.js
@@ -8,7 +8,7 @@ var viewer = new Cesium.Viewer('cesiumContainer');
     //Cesium.Math.setRandomNumberSeed(0);
     viewer.dataSources.removeAll();
     
-    var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200403.geojson');
+    var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200424.geojson');
     //viewer.dataSources.add(promise);
 
     promise.then(function(dataSource) {
@@ -54,11 +54,61 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 });
 
 
+document.getElementById("人口比率").addEventListener("click",function () {
+  //Cesium.Math.setRandomNumberSeed(0);
+  viewer.dataSources.removeAll();
+  
+  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200425.geojson');
+  //viewer.dataSources.add(promise);
+
+  promise.then(function(dataSource) {
+      viewer.dataSources.add(dataSource);
+      viewer.flyTo(dataSource);
+      //Get the array of entities
+      var entities = dataSource.entities.values;
+
+      var colorHash = {};
+      for (var i = 0; i < entities.length; i++) {
+          //For each entity, create a random color based on the state name.
+          //Some states have multiple entities, so we store the color in a
+          //hash so that we use the same color for the entire state.
+          var entity = entities[i];
+          var name = entity.name;
+          var color = colorHash[name];
+          if (entity.properties.人口比率== 0) {
+              color = Cesium.Color.fromRandom({
+                alpha : 255.0
+            });
+              colorHash[name] = color;
+          }else{
+                color =   Cesium.Color.CRIMSON;
+            colorHash[name] = color;
+          }
+      
+          //Set the polygon material to our random color.
+          entity.polygon.material = color;
+          //Remove the outlines.
+          entity.polygon.outline = false;
+
+          //Extrude the polygon based on the state's population.  Each entity
+          //stores the properties for the GeoJSON feature it was created from
+          //Since the population is a huge number, we divide by 50.
+          entity.polygon.extrudedHeight = entity.properties.人口比率 *1000  ;
+      }
+  }).otherwise(function(error){
+    //Display any errrors encountered while loading.
+    window.alert(error);
+});
+//   viewer.dataSources.add(Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10.geojson'));
+
+});
+
+
 document.getElementById("現在は入院等").addEventListener("click",function () {
   //Cesium.Math.setRandomNumberSeed(0);
   viewer.dataSources.removeAll();
   
-  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200403.geojson');
+  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200424.geojson');
   //viewer.dataSources.add(promise);
 
   promise.then(function(dataSource) {
@@ -107,7 +157,7 @@ document.getElementById("退院者").addEventListener("click",function () {
   //Cesium.Math.setRandomNumberSeed(0);
   viewer.dataSources.removeAll();
   
-  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200403.geojson');
+  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200424.geojson');
   //viewer.dataSources.add(promise);
 
   promise.then(function(dataSource) {
@@ -156,7 +206,7 @@ document.getElementById("死亡者").addEventListener("click",function () {
   //Cesium.Math.setRandomNumberSeed(0);
   viewer.dataSources.removeAll();
   
-  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200403.geojson');
+  var promise = Cesium.GeoJsonDataSource.load('https://nbayashi.github.io/cesium/covid/Source/buffer10_200424.geojson');
   //viewer.dataSources.add(promise);
 
   promise.then(function(dataSource) {
